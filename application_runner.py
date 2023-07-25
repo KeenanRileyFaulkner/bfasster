72d644fa350b23815897323b8fe36eb00acabb1d
KeenanRileyFaulkner <keenan.r.faulkner@gmail.com>
2023-07-25 16:24:37 -0600

    added wrapping class to create and run ninja file in one go

---
+++ application_runner.py
@@ -0,0 +1,27 @@
+import pathlib
+import subprocess
+from flows.flow import get_flow
+
+
+class ApplicationRunner:
+    def __init__(self):
+        self.root = pathlib.Path(__file__).parent
+
+    def run(self):
+        # create and execute a yaml parser
+        self.design = "add4"
+        flow = "vivado"
+
+        # run the flow
+        flow = get_flow(flow)()
+        flow.create()
+
+        # run the ninja file
+        self.run_ninja()
+
+    def run_ninja(self):
+        subprocess.Popen("ninja", cwd=self.root / "designs" / self.design / "out")
+
+
+if __name__ == "__main__":
+    ApplicationRunner().run()
